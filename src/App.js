ae5c73e8f1aa90793cea86c75343863ad649e2dd
Nihal-P <pate4360@mylaurier.ca>
2021-04-11 13:49:18 -0500

    added Sidebar compnent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Intro from './Intro'
+import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="App">
-      <Intro/>
+      <div className="content">
+        <Intro/>
+      </div>
+      <Sidebar/>
     </div>
   );
 }
