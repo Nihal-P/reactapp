f62ae39a60fc8c24a34e4ad814a6d4c3e734775a
Nihal-P <pate4360@mylaurier.ca>
2021-04-11 13:47:24 -0500

    added Sidbar js

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import './Sidebar.css';
+const Sidebar = () => {
+    return ( 
+        <div id="sidenav">
+            <a href="/">/home</a>
+            <a href="/">/about</a>
+            <a href="/">/experience</a>
+            <a href="/">/software-creations</a>
+        </div>
+     );
+}
+ 
+export default Sidebar;
