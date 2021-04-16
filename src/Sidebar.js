558de310e8c8f285860551228e4c5fcb357c7d1c
Nihal-P <pate4360@mylaurier.ca>
2021-04-15 19:30:50 -0500

    added sidebar logos

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,21 @@
 import './Sidebar.css';
+import EmailRoundedIcon from '@material-ui/icons/EmailRounded';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 const Sidebar = () => {
     return ( 
         <div id="sidenav">
-            <a href="/">/home</a>
-            <a href="/">/about</a>
-            <a href="/">/experience</a>
-            <a href="/">/software-creations</a>
+            <a href="/" className="sidelinks">/home</a>
+            <a href="/" className="sidelinks">/about</a>
+            <a href="/" className="sidelinks">/experience</a>
+            <a href="/" className="sidelinks">/software-creations</a>
+
+            <div className="sidebarlogos">
+                <a href="/"><EmailRoundedIcon style={{fontSize:22}} className="sidelogo"></EmailRoundedIcon></a>
+                <a href="/"><GitHubIcon style={{fontSize:20}} className="sidelogo"></GitHubIcon></a>
+                <a href="/"><LinkedInIcon style={{fontSize:22}} className="sidelogo"></LinkedInIcon></a>
+            </div>    
         </div>
      );
 }
