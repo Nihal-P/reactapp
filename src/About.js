c882b00e99da3518a7040eba812068ae267d955a
Nihal-P <pate4360@mylaurier.ca>
2021-04-16 00:12:42 -0500

    added about title and divide line

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import './About.css'
+
+
+const About = () => {
+    return ( 
+        <div id="about">
+            <div className="sectionHeader">
+                <span className="aboutTitle">/ about me<div className="divideLine"></div></span>
+            </div>
+            <div className="abtdsc">
+                <p>lipsom Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ea necessitatibus neque numquam eaque distinctio quasi animi quidem sapiente eos voluptate, dignissimos aperiam qui aliquid doloribus temporibus! Saepe, illo accusamus.</p>
+                <p>lissom Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nisi distinctio vero voluptates, consequuntur ipsa quo corporis, illo inventore reprehenderit blanditiis aperiam, labore consequatur harum quasi autem ducimus quibusdam! Doloribus.</p>
+                <p>Here are some technologies I have been working with:</p>
+            </div>
+            <div className="abtimg">
+                <img src="/"></img>
+            </div>
+        </div>
+     );
+}
+ 
+export default About;
