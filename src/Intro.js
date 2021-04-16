68ec841e9772d1f674f31997595be3f3066a6985
Nihal-P <pate4360@mylaurier.ca>
2021-04-15 19:30:36 -0500

    Fixed overlapping bugs with email link

--- src/Intro.js
+++ src/Intro.js
@@ -5,16 +5,15 @@ import Typist from 'react-typist';
 const Intro = () => {
     return ( 
         <div id="intro">
-            <Typist avgTypingDelay={120} cursor={{ show: false,hideWhenDone: true }}>
+            <Typist avgTypingDelay={120} >
             <h2 className="nameLine">Hi, <span className="name">Nihal Patel</span> here.</h2>
             </Typist>
-           
             <h4 className="stuff">I create stuff sometimes.</h4>
             <p className="intropar">I am a first year student at Wilfrid Laurier University. 
             I have profound interest in Computer Science with music on the side. 
             Over the period of time I have developed niche skills of using google and stack-overflow which helped me <span className="strikedout">survive</span> grow.
             </p>
-            <a href="/" className="emailink"><EmailRoundedIcon style={{fontSize:25}}></EmailRoundedIcon><span className="contact-text">&#8205; Say hello</span></a>
+            <a href="/" className="emailink"><EmailRoundedIcon style={{fontSize:25, color:"#64ffda"}}></EmailRoundedIcon><span className="contact-text">&#8205; Say hello</span></a>
         </div>
      );
 }
