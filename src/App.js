206e156dd29b06629a3081179270b734ffcd0979
Nihal-P <pate4360@mylaurier.ca>
2021-04-16 22:15:48 -0500

    remvd template styles, added some app common style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Intro from './Intro'
 import Sidebar from './Sidebar';
+import About from './About';
 function App() {
   return (
     <div className="App">
-      <div className="content">
+      <div id="content">
         <Intro/>
+        <About/>
       </div>
       <Sidebar/>
     </div>
