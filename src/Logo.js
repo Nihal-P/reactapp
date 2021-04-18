79ad1c927946d2633b2f6ba5a18bce86f4b31457
Nihal-P <pate4360@mylaurier.ca>
2021-04-17 20:16:09 -0500

    created side logo

---
+++ src/Logo.js
@@ -0,0 +1,11 @@
+import logo from "./nweblogo.png"
+import "./Logo.css"
+const Logo = () => {
+    return ( 
+        <div>
+            <img src={logo} id="sidelogo" alt="my initial logo"></img>
+        </div>
+     );
+}
+ 
+export default Logo;
