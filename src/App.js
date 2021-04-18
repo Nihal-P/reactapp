1435c6700a495324d472d0a4a1c86f0ab9d95c8f
Nihal-P <pate4360@mylaurier.ca>
2021-04-17 22:11:07 -0500

    inserted a new side logo component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import Intro from './Intro'
 import Sidebar from './Sidebar';
 import About from './About';
+import Logo from './Logo';
 function App() {
   return (
     <div className="App">
+      <Logo></Logo>
       <div id="content">
         <Intro/>
         <About/>
