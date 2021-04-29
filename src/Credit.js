fff925b9be2396ed5bc0c95e0ecfa5c24777d2f8
Nihal-P <pate4360@mylaurier.ca>
2021-04-29 18:30:53 -0500

    Created/Finished a Credit section

---
+++ src/Credit.js
@@ -0,0 +1,13 @@
+import "./Credit.css"
+const Credit = () => {
+    return ( 
+        <div id="credits">
+            <div className="endCredits">
+                <div>Built and Designed by Nihal Patel</div>
+                <div>All rights reserved. ©</div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Credit;
