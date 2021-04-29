a15b531da79a3b4af80382019fb2e1adfb7ae4ed
Nihal-P <pate4360@mylaurier.ca>
2021-04-29 18:31:56 -0500

    Added Experience, project and Credit comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Intro from './Intro'
 import Sidebar from './Sidebar';
 import About from './About';
 import Logo from './Logo';
+import Experience from './Experience'
+import Projects from './Projects'
+import Credit from './Credit'
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +14,9 @@ function App() {
       <div id="content">
         <Intro/>
         <About/>
+        <Experience/>
+        <Projects/>
+        <Credit/>
       </div>
       <Sidebar/>
     </div>
